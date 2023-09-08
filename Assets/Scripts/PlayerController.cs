2a052f44d87cc13ae02a7b64f015da5847cf3fa0
Julian Gaitan <juliangaitan_h@hotmail.com>
2023-09-08 12:13:53 -0500

    Add dive and run mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,10 +9,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float gradesMultiply;
     float movX, movZ;
     [SerializeField] float speed;
+    [SerializeField]  private float speedMultiplier;
     [SerializeField] Rigidbody rbPablito;
     Vector3 moveInput;
     Quaternion rotateInput;
-   
+
+    [Header("Slide behavior")]
+    [SerializeField] private float slideForce = 5f;
+    [SerializeField] private float slideRotateSpeed = 30f;
+    private bool isRunning = false;
+    private bool isSliding = false;
+    private bool isJumping = false;
     void Start()
     {
         pablitoTransform = GetComponent<Transform>();
@@ -24,12 +31,19 @@ public class PlayerController : MonoBehaviour
     {
         movX = Input.GetAxis("Horizontal");
         movZ = Input.GetAxis("Vertical");
+        isRunning = Input.GetKey(KeyCode.LeftShift);
+        isJumping = Input.GetKey(KeyCode.Space);
 
+        if (!isSliding && Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isSliding = true;
+            StartCoroutine(SlidingCoroutine());
+        }
     }
     private void FixedUpdate()
     {
 
-        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical") && !isSliding)
         {
             ControlMovement();
         }
@@ -37,12 +51,45 @@ public class PlayerController : MonoBehaviour
     }
     void ControlMovement()
     {
+        if (isRunning)
+        {
+            speedMultiplier = 2.25f;
+        }
+        else
+        {
+            speedMultiplier = 1f;
+        }
         moveInput = new Vector3(movX, moveInput.y, movZ);
         Vector3 directionToMove = rbPablito.rotation * moveInput;
         rotateInput = Quaternion.Euler(rotateInput.x, movX * gradesMultiply, rotateInput.z);
 
-        rbPablito.MovePosition(rbPablito.position + directionToMove * speed * Time.fixedDeltaTime);
+        rbPablito.MovePosition(rbPablito.position + directionToMove * (speed * speedMultiplier)  * Time.fixedDeltaTime);
         rbPablito.MoveRotation(rbPablito.rotation * rotateInput);
 
+    }
+
+    private IEnumerator SlidingCoroutine()
+    {
+        isSliding = true;
+
+        // Apply force to simulate sliding
+        rbPablito.AddForce(transform.forward * slideForce, ForceMode.Impulse);
+
+        // Calculate the target rotation
+        Quaternion targetRotation = Quaternion.Euler(90f, transform.eulerAngles.y, transform.eulerAngles.z);
+
+        float startTime = Time.time;
+        float journeyLength = 1f; // Adjust this value to control the rotation speed
+
+        while (Time.time < startTime + journeyLength)
+        {
+            float distanceCovered = (Time.time - startTime) / journeyLength;
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, distanceCovered);
+            yield return null;
+        }
+
+        transform.rotation = targetRotation; // Ensure the final rotation is exactly 90 degrees.
+
+        isSliding = false;
     }
 }
