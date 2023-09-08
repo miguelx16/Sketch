11e30878e5d728f264f3504d2be684cbfd4c4817
jdgonzalezd1 <60698448+jdgonzalezd1@users.noreply.github.com>
2023-09-08 17:17:41 -0500

    Commit 2: Enemy Added

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
 {
-
+    [SerializeField] float speed;
+    UiFunctions globalFunctions;
+    Vector3 zMov;
     // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        
+        globalFunctions = FindAnyObjectByType<UiFunctions>();
+        zMov = new Vector3(0, 0, 1);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        EnemyMovement(zMov);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        zMov = zMov * -1;
+        if (collision.gameObject.name == "Pablito")
+        {
+            Debug.Log("Game Over");
+            globalFunctions.ChangeScene("InitialMenu");
+        }
+    }
+
+    void EnemyMovement(Vector3 zMov)
+    {
+        transform.Translate(zMov * speed * Time.deltaTime);
     }
 }
